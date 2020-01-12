c1289bd170089f73967c5f5aea723e8a18a04942
limeng000111 <3176832432@qq.com>
2020-01-13 07:12:12 +0800

    提交problem06(home)

---
+++ test-for-money/python06.py
@@ -0,0 +1,21 @@
+'''
+data:2020-01-13
+'''
+#有一组数列，2/1，3/2，5/3，8/5...求前20项之和
+from functools import reduce
+def sequence():
+    l = []
+    a = 2
+    b = 1
+    s = 0
+    for i in range(1,21):
+        s += (a/b)
+        l.append('%s/%s'%(a,b))
+        a , b = a+b , a
+    sum1 = reduce(lambda x,y:x+y,l)
+    #print('数列的内容为：',l)
+   # print('数列的前20项和:',sum1)
+    print('+'.join(str(i) for i in l),end='\n')
+
+    print('=%.2f'%s)
+sequence()
