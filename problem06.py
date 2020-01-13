30a82da6c36c7cf46c29a557ba6537703c1267dc
limeng000111 <3176832432@qq.com>
2020-01-13 21:26:20 +0800

    公司完成第2到题目：n！的和

--- problem06.py
+++ problem06.py
@@ -18,4 +18,32 @@ def sequence():
     print('+'.join(str(i) for i in l),end='\n')#学会使用.join的用法
 
     print('=%.2f'%s)
-sequence()
+# sequence()
+
+#递归当时求5i
+# def fact(i):
+# #     sum = 0
+# #     if i == 0:
+# #         sum = 1
+# #     else:
+# #         sum = i * fact(i-1)
+# #     # print(sum)
+# #     return sum
+# # for j in range(5):
+# #     print(j,fact(j))
+
+
+def fact(i):
+    sum = 0
+    if i == 0:
+        sum =1
+    else:
+        sum = i * fact(i-1)
+    return sum#返回只是一个函数
+    #print(sum)
+
+#注意，递归函数需要调用才能生效
+a = []
+for j in range(5):
+    a.append(fact(j))
+print('n!的总和为%s'%reduce(lambda x,y:x+y,a))
